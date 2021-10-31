21d229d4b9ca6d70587fde26173be7fdc1435dee
Viktoriia Huk <relax16011991@gmail.com>
2021-10-31 21:27:15 +0200

    + задание 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    startBtn : document.querySelector('[data-start]'),
+    stopBtn : document.querySelector('[data-stop]'),
+}
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtn.addEventListener('click', onStartBtnClick)
+refs.stopBtn.addEventListener('click', onStopBtnClick)
+
+let intervalId = null;
+
+function onStartBtnClick() {
+    intervalId = setInterval(
+        () => {
+            document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+    refs.startBtn.setAttribute('disabled', true);
+}
+    
+function onStopBtnClick() {
+    clearInterval(intervalId);
+    refs.startBtn.removeAttribute('disabled')
+    
+}
