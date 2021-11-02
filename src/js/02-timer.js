ed77e87df0c786d570b1b972763739a06f748461
Viktoriia Huk <relax16011991@gmail.com>
2021-11-02 13:25:31 +0200

    добавляет библиотеку notifliх

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
     startBtn: document.querySelector('[data-start]'),
@@ -20,18 +21,39 @@ flatpickr('#datetime-picker', {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    //   console.log(selectedDates[0]);
+      console.log(selectedDates[0]);
       selectedDate = selectedDates[0];
       currentDate = new Date();
-
-    if (selectedDate.getTime() < currentDate.getTime()) {
-        refs.startBtn.setAttribute('disabled', true);
-        alert('Please choose a date in the future');
-        return;
-    }
-      
-      refs.startBtn.removeAttribute('disabled');
-      
-  },
+if(selectedDate.getTime() < currentDate.getTime()){
+            Notify.failure('Please choose a date in the future');
+              refs.startBtn.setAttribute('disabled', true)
+            return;
+        }
+       refs.startBtn.removeAttribute('disabled');
+    },
 });
+
+
+ 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+
 
