b3299663f03f7d50b96f068a3b082ccbfffe1927
Viktoriia Huk <relax16011991@gmail.com>
2021-11-03 15:14:48 +0200

    + Задание 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -20,4 +20,28 @@ function createPromise(position, delay) {
   });
 };
 
+ 
+ const makePromises = () => {
+  let amountNum = Number(refs.inputAmount.value);
+  let delayNum = Number(refs.inputDelay.value);
+  let stepNum = Number(refs.inputStep.value);
+   
+  for (let i = 1; i <= amountNum; i++) {
+    createPromise(i, delayNum)
+      .then(({ position, delay }) => {
+       Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+       Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      })
+      delayNum += stepNum;
+  }
+  
+}
+ 
+const onFormsabmit = (evt) => {
+  evt.preventDefault();
+  makePromises()
+}
 
+refs.form.addEventListener('submit', onFormsabmit)
